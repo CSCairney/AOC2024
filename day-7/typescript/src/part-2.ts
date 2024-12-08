c2ae9a5bfc991068a7df2641e9224789cae51ecf
Charles Cairney <107991366+CSCairney@users.noreply.github.com>
2024-12-08 15:13:24 +0000

    Day 7 - Part 2 - typescript solution

--- day-7/typescript/src/part-2.ts
+++ day-7/typescript/src/part-2.ts
@@ -1,12 +1,20 @@
 import {readFileData} from "../helpers/reader";
-import {calculateInfiniteLoopCount, parseData} from "../helpers/transformers";
+import {calibrateResult, calibrateResultWithCombine, parseData} from "../helpers/transformers";
 
 const data = readFileData("../data/input.txt");
 if (data) {
     const parsedData = parseData(data);
+    let total = 0;
 
     if (parsedData) {
-        console.log("Part Two Answer:", calculateInfiniteLoopCount(parsedData));
+        parsedData.map((value) => {
+            const output = calibrateResultWithCombine(value.answer, value.values);
+            if (output) {
+                total += value.answer
+            }
+        })
+
+        console.log("Part Two Answer:", total);
     } else {
         console.log("No valid data found.");
     }
